058df94fe6c7ec40fcef2043166fcaccfe80de76
U9D3RTAK3R <104409038+U9D3RTAK3R@users.noreply.github.com>
2025-07-14 14:24:15 +0530

    feat: Backend Routes Setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-require("dotenv").config();
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -14,6 +16,14 @@ app.use(
 );
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+//Serve Uploads Faster
+app.use("/uploads",express.static(path.join(__dirname, "uploads")));
+
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
